9264451467b3da6f4520e8cd3de1a1f844f3f0d6
Mapelujo <olamideiyanda18@gmail.com>
2024-08-11 18:28:15 +0100

    implemented lazy loading with the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,44 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-import Login from "./pages/Login";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+// import PageNotFound from "./pages/PageNotFound";
+// dist/assets/index-d9c85721.css   29.96 kB │ gzip:   5.07 kB
+// dist/assets/index-5e431094.js   510.44 kB │ gzip: 149.21 kB
+
+// LAZY LOADING THE PAGES
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
+        <Suspense fallback={<SpinnerFullPage />}>
           <Routes>
             <Route index element={<Homepage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
 
             {/* CREATING INDEX & NESTED ROUTES */}
             <Route
@@ -42,9 +58,9 @@ function App() {
               <Route path="cities/:id" element={<City />} />
             </Route>
 
-            <Route path="login" element={<Login />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
+        </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
